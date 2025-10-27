c490931b9265d8b34557853766ac2854899c221a
木i梨 <14946600+mui-pear@user.noreply.gitee.com>
2025-10-27 15:18:26 +0800

    feat: tring Jsdelivr and PicGo to loading picture.

--- source/_posts/docs/about_me.md
+++ source/_posts/docs/about_me.md
@@ -2,5 +2,6 @@
 title: 测试
 date: 2025-10-16 15:55:19
 tags: test
+reward: false
 ---
 这是一个用于测试的页面。
--- themes/shoka/scripts/helpers/engine.js
+++ themes/shoka/scripts/helpers/engine.js
@@ -28,7 +28,7 @@ const randomBG = function(count = 1, image_server = null, image_list = []) {
     if (img.startsWith('//') || img.startsWith('http')) {
       return img
     } else {
-      return 'https://tva'+randomServer+'.sinaimg.cn/'+size+'/'+img
+      return 'https://github.com/muyuliyan/myblog_figureBed/tree/main/covers'+img
     }
   }
 
